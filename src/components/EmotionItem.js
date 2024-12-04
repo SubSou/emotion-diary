3683f8bc963c2d2163780a83d1c458aaad19af47
unknown <sdlking@naver.com>
2024-12-04 13:59:15 +0900

    emotion-diary

---
+++ src/components/EmotionItem.js
@@ -0,0 +1,15 @@
+const EmotionItem = ({
+  emotion_id,
+  emotion_img,
+  emotion_descript,
+  onClick,
+}) => {
+  return (
+    <div className="EmotionItem" onClick={() => onClick(emotion_id)}>
+      <img src={emotion_img} />
+      <span>{emotion_descript}</span>
+    </div>
+  );
+};
+
+export default EmotionItem;
---
+++ src/pages/New.js
@@ -0,0 +1,14 @@
+import DiaryEditor from "../components/DiaryEditor";
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
+
+const New = () => {
+  return (
+    <div>
+      <DiaryEditor />
+    </div>
+  );
+};
+
+export default New;
